24423268cf8211171460268643516e71654754d3
JimeFlow <jimenafloresca07@gmail.com>
2025-11-19 16:52:13 -0600

    FINAL DB

--- BiblioSolaris.sql
+++ BiblioSolaris.sql
@@ -61,6 +61,18 @@ CREATE TABLE Libro(
     FOREIGN KEY (Id_Estado) REFERENCES Estado(Id_Estado)
 );
 
+-- EJEMPLAR -- NUEVA
+CREATE TABLE Ejemplar (
+    Id_Ejemplar INT IDENTITY(1,1) PRIMARY KEY,
+    CodigoEjemplar VARCHAR(20) UNIQUE NOT NULL,
+    Id_Libro INT NOT NULL,
+    Estado VARCHAR(50) NOT NULL DEFAULT('Disponible'),
+    Ubicacion VARCHAR(100) NULL,
+    Fecha_Registro DATETIME2 DEFAULT(GETDATE()),
+    FOREIGN KEY (Id_Libro) REFERENCES Libro(Id_Libro)
+);
+-------------------------------------------------------------------------------------------------------
+
 CREATE TABLE Libro_Etiquetas( --- NUEVA
     Id_Libro INT,
     Id_Categoria INT,
@@ -158,3 +170,393 @@ CREATE TABLE Logs( -- Registro Diario
     Id_Usuario INT,
     FOREIGN KEY (Id_Usuario) REFERENCES Usuario (Id_Usuario)
 );
+
+
+
+/* ****************************************************************************************************
+   ********************************* MODIFICACIONES DE LA BD - TABLAS *********************************
+   **************************************************************************************************** */
+USE BiblioSolaris;
+GO
+
+-- ALTER TABLE AGREGAR COLUMNA IDENTIFICACION
+ALTER TABLE Usuario ADD Identificacion VARCHAR(9);
+ALTER TABLE Usuario ADD CONSTRAINT IDENTIFICACION_UNIQUE UNIQUE(Identificacion);
+-------------------------------------------------------------------------------------------------------
+
+-- INSERT DE LOS ROLES 
+INSERT INTO Rol(Tipo_Rol) VALUES
+('Administrador'), ('Cliente');
+-------------------------------------------------------------------------------------------------------
+
+-- ALTER TABLE HACER MAS GRANDE LAS CONTRASENAS
+ALTER TABLE Usuario ALTER COLUMN Contrasena VARCHAR(255);
+-------------------------------------------------------------------------------------------------------
+
+-- ALTER TABLE - ESTADO A FK ID_ESTADO
+ALTER TABLE Movimiento ADD Id_Estado INT;
+ALTER TABLE Movimiento ADD FOREIGN KEY (Id_Estado) REFERENCES Estado(Id_Estado);
+-------------------------------------------------------------------------------------------------------
+
+-- ALTER TABLE - VALIDAR QUE RATING ESTE ENTRE 1 Y 5
+ALTER TABLE Comentario
+ADD CONSTRAINT CK_Rating CHECK (Rating BETWEEN 1 AND 5);
+-------------------------------------------------------------------------------------------------------
+
+-- PREGUNTAS FRECUENTES - FREQUENTLY ASKED QUESTIONS
+CREATE TABLE FAQ (
+    Id_FAQ INT IDENTITY(1,1) PRIMARY KEY,
+    Pregunta VARCHAR(255),
+    Respuesta TEXT,
+    Estado BIT -- 1 = Activa / 0 = Oculta
+);
+-------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABLA ESTADO
+INSERT INTO Estado (Estado)
+VALUES ('Activo'), ('Inactivo'), ('Disponible'), ('Reservado'), ('Pendiente'),('En Proceso'), ('Completado');
+-------------------------------------------------------------------------------------------------------
+
+/* ****************************************************************************************************
+   ************************************ PROCEDIMIENTOS ALMACENADOS ************************************
+   **************************************************************************************************** */
+
+-- INICIAR SESION -------------------------------------------------------------------------------------
+CREATE PROCEDURE ObtenerUsuarioPorCorreo
+	@Correo VARCHAR(75)
+AS
+BEGIN
+	SELECT Id_Usuario,			
+			Nombre,
+			Apellidos,
+			Identificacion,
+			Correo,
+			Contrasena,
+			Telefono,
+			Estado,
+			U.Id_Rol,
+			R.Tipo_Rol
+			FROM Usuario U INNER JOIN Rol R ON U.Id_Rol = R.Id_Rol
+			WHERE Correo = @Correo AND Estado = 1
+END
+
+
+-- REGISTRARSE -------------------------------------------------------------------------------------
+CREATE PROCEDURE RegistroUsuario
+	@Nombre VARCHAR(50),
+	@Apellidos VARCHAR(100),
+	@Identificacion VARCHAR(9),
+	@Correo VARCHAR(75),
+	@Contrasena VARCHAR(100),
+	@Telefono VARCHAR(15)
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM Usuario WHERE Correo = @Correo OR Telefono = @Telefono)
+	BEGIN
+	SELECT 0 AS Resultado
+	RETURN
+	END
+	
+	DECLARE @Id_Rol INT = 2
+	DECLARE @Estado BIT = 1
+
+	INSERT INTO dbo.Usuario(Nombre, Apellidos, Identificacion, Correo, Contrasena, Telefono, Id_Rol, Estado)
+	VALUES (@Nombre, @Apellidos, @Identificacion, @Correo, @Contrasena, @Telefono, @Id_Rol, @Estado)
+	SELECT 1 AS Resultado
+END
+
+
+/* ****************************************************************************************************
+   ********************************** NUEVAS TABLAS Y PROCEDIMIENTOS **********************************
+   **************************************************************************************************** */
+
+-- TABLA PARA REGISTRAR ERRORES DESDE EL API ----------------------------------------------------------
+CREATE TABLE Error(
+	ConsecutivoError INT PRIMARY KEY NOT NULL,
+	Id_Usuario INT NOT NULL,
+	Mensaje VARCHAR(MAX) NOT NULL,
+	Origen VARCHAR(80) NOT NULL,
+	FechaHora DATETIME NOT NULL
+);
+
+DROP TABLE Error;
+
+CREATE TABLE Error(
+	ConsecutivoError INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Id_Usuario INT NOT NULL,
+	Mensaje VARCHAR(MAX) NOT NULL,
+	Origen VARCHAR(80) NOT NULL,
+	FechaHora DATETIME NOT NULL
+)
+--------------------------------------------------------------------------------------------------------
+
+-- SP PARA ERRORES -------------------------------------------------------------------------------------
+CREATE PROCEDURE [dbo].[RegistrarError]
+	@Id_Usuario INT,
+	@MensajeError VARCHAR(MAX),
+	@OrigenError VARCHAR(80)
+AS
+BEGIN
+	INSERT INTO Error (Id_Usuario, Mensaje, Origen, FechaHora)
+	VALUES (@Id_Usuario, @MensajeError, @OrigenError, GETDATE())
+END
+
+-- SP PARA REGISTRAR USUARIOS (ADMIN) -------------------------------------------------------------------
+CREATE PROCEDURE RegistroUsuarioAdmin
+	@Nombre VARCHAR(50),
+	@Apellidos VARCHAR(100),
+	@Identificacion VARCHAR(9),
+	@Correo VARCHAR(75),
+	@Contrasena VARCHAR(100),
+	@Telefono VARCHAR(15),
+	@Id_Rol INT
+AS
+BEGIN
+	DECLARE @Estado BIT = 1
+	IF NOT EXISTS(SELECT 1 FROM Usuario WHERE Correo = @Correo OR Identificacion = @Identificacion OR Telefono = @Telefono)
+	BEGIN
+		INSERT INTO dbo.Usuario(Nombre, Apellidos, Identificacion, Correo, Contrasena, Telefono, Id_Rol, Estado)
+		VALUES (@Nombre, @Apellidos, @Identificacion, @Correo, @Contrasena, @Telefono, @Id_Rol, @Estado)
+
+		SELECT @@IDENTITY 'Id_Usuario'
+	END
+	SELECT 0 'Id_Usuario'
+END
+
+-- SP PARA LISTAR USUARIOS -------------------------------------------------------------------------------------
+CREATE PROCEDURE ListarUsuarios
+AS
+BEGIN
+	SELECT Id_Usuario, Nombre, Apellidos, Correo, Telefono, U.Id_Rol, Estado, Identificacion, R.Tipo_Rol
+	FROM Usuario U INNER JOIN Rol R ON U.Id_Rol = R.Id_Rol
+END
+
+-- SP PARA ACTUALIZAR USUARIO -------------------------------------------------------------------------------------
+CREATE PROCEDURE ActualizarUsuarioAdmin
+	@Id_Usuario INT,
+	@Nombre VARCHAR(50),
+	@Apellidos VARCHAR(100),
+	@Identificacion VARCHAR(9),
+	@Correo VARCHAR(75),
+	@Telefono VARCHAR(15),
+	@Id_Rol INT,
+	@Estado BIT
+AS
+BEGIN
+	UPDATE Usuario
+	SET Identificacion= @Identificacion,
+	Nombre = @Nombre,
+	Apellidos = @Apellidos,
+	Correo = @Correo,
+	Telefono = @Telefono,
+	Id_Rol = @Id_Rol,
+	Estado = @Estado
+	WHERE Id_Usuario = @Id_Usuario
+END
+
+-- SP PARA CAMBIAR CONTRASEÑA -------------------------------------------------------------------------------------
+CREATE PROCEDURE CambiarContrasena
+	@Id_Usuario INT,
+	@Contrasena VARCHAR(100)
+AS
+BEGIN
+	UPDATE Usuario
+	SET Contrasena = @Contrasena
+	WHERE Id_Usuario = @Id_Usuario
+END
+
+--------------------------------------------------------------------------------------------------------
+---------------------------------------- "OLVIDE MI CONTRASEÑA" ----------------------------------------
+--------------------------------------------------------------------------------------------------------
+-- SP PARA VALIDAR USUARIO ---------------------------------------------------------------------------------
+CREATE PROCEDURE ValidarUsuario
+	@Correo VARCHAR(75)
+AS
+BEGIN
+	SELECT Id_Usuario,
+			Identificacion,
+			Nombre,
+			Apellidos,
+			Correo,
+			Contrasena,
+			Telefono,
+			Estado,
+			Id_Rol
+			FROM Usuario
+			WHERE Correo = @Correo AND Estado = 1
+END
+
+-- SP PARA CAMBIAR CONTRASEÑA -------------------------------------------------------------------------------------
+CREATE PROCEDURE ActualizarContrasena
+	@Id_Usuario INT,
+	@Contrasena VARCHAR(255)
+AS
+BEGIN
+	UPDATE Usuario
+	SET Contrasena = @Contrasena
+	WHERE Id_Usuario = @Id_Usuario
+END
+
+-- SP PARA EDITAR PERFIL -------------------------------------------------------------------------------------
+CREATE PROCEDURE ActualizarPerfil
+	@Id_Usuario INT,
+	@Nombre VARCHAR(50),
+	@Apellidos VARCHAR(100),
+	@Identificacion VARCHAR(9),
+	@Correo VARCHAR(75),
+	@Telefono VARCHAR(15)
+AS
+BEGIN
+
+	IF EXISTS(SELECT 1 FROM Usuario WHERE Id_Usuario <> @Id_Usuario AND(Identificacion = @Identificacion OR Correo = @Correo 
+	OR Telefono = @Telefono))
+	BEGIN
+		SELECT 0 AS Resultado
+		RETURN
+	END
+		UPDATE Usuario
+		SET Identificacion= @Identificacion,
+		Nombre = @Nombre,
+		Apellidos = @Apellidos,
+		Correo = @Correo,
+		Telefono = @Telefono
+		WHERE Id_Usuario = @Id_Usuario
+	
+		SELECT 1 AS Resultado
+END
+
+--------------------------------------------------------------------------------------------------------
+-- SP PARA LISTAR ROLES -------------------------------------------------------------------------------------
+
+CREATE PROCEDURE ListarRoles
+AS
+BEGIN
+	SELECT Id_Rol, Tipo_Rol
+	FROM Rol 
+END
+
+-- SP PARA ELIMINAR USUARIO ADMIN -------------------------------------------------------------------------------------
+CREATE PROCEDURE EliminarUsuario
+	@Id_Usuario INT
+AS
+BEGIN
+	DELETE FROM Usuario WHERE Id_Usuario = @Id_Usuario
+END
+
+-- SP PARA OBTENER USUARIO X ID -------------------------------------------------------------------------------------
+CREATE PROCEDURE ObtenerUsuarioPorId
+	@Id_Usuario INT
+AS
+BEGIN
+	SELECT Id_Usuario, Nombre, Apellidos, Correo, Telefono, U.Id_Rol, R.Tipo_Rol, Estado, Identificacion 
+	FROM Usuario U INNER JOIN Rol R ON U.Id_Rol = R.Id_Rol WHERE Id_Usuario = @Id_Usuario
+END
+--------------------------------------------------------------------------------------------------------
+
+/* ****************************************************************************************************
+   ********************************** PROCEDIMIENTOS ALMACENADOS P4 ***********************************
+   **************************** RESERVAS, NOTIFICACIONES, COMENTARIOS, FAQ ****************************
+   **************************************************************************************************** */
+
+-- RESERVAS DE LIBROS 
+-- Crear reserva -------------------------------------------------------------------------------------
+CREATE PROCEDURE CrearReserva
+    @Id_Usuario INT,
+    @Id_Libro INT,
+    @Fecha_Reserva DATETIME2,
+    @Fecha_Expiracion DATETIME2,
+    @Id_Estado INT -- Pendiente
+AS
+BEGIN
+    INSERT INTO Movimiento (Tipo, Fecha, Fecha_Vencimiento, Id_Usuario, Id_Libro, Id_Estado)
+    VALUES ('RESERVA', @Fecha_Reserva, @Fecha_Expiracion, @Id_Usuario, @Id_Libro, @Id_Estado)
+END
+
+-- Cancelar reserva -------------------------------------------------------------------------------------
+CREATE PROCEDURE CancelarReserva
+    @Id_Movimiento INT,
+    @Id_Estado INT -- Cancelada
+AS
+BEGIN
+    UPDATE Movimiento
+    SET Id_Estado = @Id_Estado
+    WHERE Id_Movimiento = @Id_Movimiento AND Tipo = 'RESERVA'
+END
+
+-- Expirar reserva -------------------------------------------------------------------------------------
+CREATE PROCEDURE ExpirarReserva
+AS
+BEGIN
+    UPDATE Movimiento
+    SET Id_Estado = (SELECT Id_Estado FROM Estado WHERE Estado = 'Expirada')
+    WHERE Tipo = 'RESERVA' AND Fecha_Vencimiento < GETDATE() AND Id_Estado <> (SELECT Id_Estado FROM Estado WHERE Estado = 'Expirada')
+END
+
+
+-- NOTIFICACIONES POR CORREO
+-- Registrar envio de correo -------------------------------------------------------------------------------------
+CREATE PROCEDURE RegistrarCorreoReserva
+    @Id_Usuario INT,
+    @Descripcion VARCHAR(150)
+AS
+BEGIN
+    INSERT INTO Logs (Fecha, Tipo_Accion, Descripcion_Accion, Modulo_Afectado, Id_Usuario)
+    VALUES (GETDATE(), 'Correo', @Descripcion, 'Reservas', @Id_Usuario)
+END
+
+
+-- COMENTARIOS Y CALIFICACIONES
+-- Agregar comentario -------------------------------------------------------------------------------------
+CREATE PROCEDURE AgregarComentario
+    @Id_Usuario INT,
+    @Id_Libro INT,
+    @Comentario VARCHAR(150),
+    @Rating INT
+AS
+BEGIN
+    INSERT INTO Comentario (Creacion, Comentario, Rating, Id_Libro, Id_Usuario)
+    VALUES (GETDATE(), @Comentario, @Rating, @Id_Libro, @Id_Usuario)
+END
+
+-- Listar comentarios por libro -------------------------------------------------------------------------------------
+CREATE PROCEDURE ListarComentariosPorLibro
+    @Id_Libro INT
+AS
+BEGIN
+    SELECT C.Id_Comentario, C.Comentario, C.Rating, C.Creacion, U.Nombre, U.Apellidos
+    FROM Comentario C
+    INNER JOIN Usuario U ON C.Id_Usuario = U.Id_Usuario
+    WHERE C.Id_Libro = @Id_Libro
+END
+
+
+-- PREGUNTAS FRECUENTES
+-- Crear pregunta -------------------------------------------------------------------------------------
+CREATE PROCEDURE CrearFAQ
+    @Pregunta VARCHAR(255),
+    @Respuesta TEXT,
+    @Estado BIT
+AS
+BEGIN
+    INSERT INTO FAQ (Pregunta, Respuesta, Estado)
+    VALUES (@Pregunta, @Respuesta, @Estado)
+END
+
+-- Listar pregunta -------------------------------------------------------------------------------------
+CREATE PROCEDURE ListarFAQsActivas
+AS
+BEGIN
+    SELECT Id_FAQ, Pregunta, Respuesta
+    FROM FAQ
+    WHERE Estado = 1
+END
+
+--------------------------------------------------------------------------------------------------------
+
+/* ****************************************************************************************************
+   ********************************** EJECUCIONES PARA REVISAR DATOS **********************************
+   **************************************************************************************************** */
+
+-- SELECT 
+SELECT TOP 1 * FROM Libro;
+--------------------------------------------------------------------------------------------------------

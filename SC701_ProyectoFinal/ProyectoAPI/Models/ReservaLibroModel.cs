b4844302b1020ede3086dac57f65d44b1ae9de1e
amandavm25 <84757258+amandavm25@users.noreply.github.com>
2025-11-19 23:39:55 -0600

    Mejorar varias en reservas libros
    Mejorar en reserva
    Funcionamiento del botón eliminar en ejemplar, pendiente revisar en libro

--- SC701_ProyectoFinal/ProyectoAPI/Models/ReservaLibroModel.cs
+++ SC701_ProyectoFinal/ProyectoAPI/Models/ReservaLibroModel.cs
@@ -6,6 +6,6 @@
         public int UsuarioId { get; set; }
         public DateTime FechaReserva { get; set; }
         public DateTime FechaVencimiento { get; set; }
-        public int EstadoReserva { get; set; } // 0: Pendiente, 1: Activa, 2: Cancelada, 3: Completada
+        public int EstadoReserva { get; set; } 
     }
 }

427d6a584b1b2865ed66f05cb518cd3a2ced8235
josuemc12 <134577865+josuemc12@users.noreply.github.com>
2025-12-17 11:33:09 -0500

    Add files via upload

---
+++ SC701_ProyectoFinal/SC701_ProyectoFinal/Models/ReservaRequestModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SC701_ProyectoFinal.Models
+{
+    public class ReservaRequestModel
+    {
+        public int Id_Movimiento { get; set; }
+        public int Id_Libro { get; set; }
+        public int Id_Ejemplar { get; set; }
+        public int Id_Usuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Fecha_Vencimiento { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Imagen_URL { get; set; } = string.Empty;
+        public string Estado { get; set; } = string.Empty;
+        public int Id_Estado { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Identificacion { get; set; } = string.Empty;
+        public bool TieneSancionActiva { get; set; }
+        public string Correo { get; set; } = string.Empty;
+
+    }
+}
+
